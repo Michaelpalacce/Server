a396372805fd3efd9e922e4672bc430cbfef918b
Michaelpalacce <stefantigro@gmail.com>
2018-05-09 15:25:23 -0700

    Fixed Router async issue. Now next middleware will only be called when the previous one says so. Added some test routes Added a 404 middleware Router now better handles middleware and is more on point! middleware is now executed in order

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const server		= require( './lib/www' );
 const env_config	= require( './lib/config/env' );
 
 server.start( env_config.port );
+
+server.add( '', ( event ) => {
+	event.render( 'index', {}, ( err ) =>{
+		if ( err )
+		{
+			event.next();
+		}
+	} );
+});
+
+server.add( ( event ) => {
+	console.log('boom');
+	if ( ! event.isFinished() )
+	{
+		event.response.setHeader( 'Content-Type', 'text/html' );
+		event.response.statusCode	= 404;
+		event.render( 'not_found', { times: 2, message:'404 NOT FOUND'}, ( err, data )=>{
+			if ( ! err && data && data.length > 0 )
+			{
+				res.end( data );
+			}
+		});
+	}
+})
