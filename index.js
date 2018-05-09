7ab69e53e1bc9a8011a7142229efaf8f64f48a71
Michaelpalacce <stefanbg123>
2018-05-09 22:39:39 +0300

    Refactored ROUTER !!! Created a templating engine! Refactored event to be better!

--- index.js
+++ index.js
@@ -5,14 +5,3 @@ const server		= require( './lib/www' );
 const env_config	= require( './lib/config/env' );
 
 server.start( env_config.port );
-
-server.add( 'test', ( event ) =>
-	{
-		event.response.end( 'MIDDLEWARE COMPLETE?' );
-	}
-);
-
-server.add( '', ( event ) =>
-{
-	event.response.end( 'Hello' );
-});
