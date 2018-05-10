00e12bf26203980ea9cf12615f947bca3c2f1591
Stefan Genov <stefan.genov@plovdivsoftware.com>
2018-05-10 14:52:36 +0300

    Documentation added and some improvements to Request Event

--- index.js
+++ index.js
@@ -6,26 +6,24 @@ const env_config	= require( './lib/config/env' );
 
 server.start( env_config.port );
 
-server.add( '', ( event ) => {
+server.add( '/', ( event ) => {
 	event.render( 'index', {}, ( err ) =>{
 		if ( err )
 		{
 			event.next();
 		}
-	} );
+	});
 });
 
 server.add( ( event ) => {
-	console.log('boom');
 	if ( ! event.isFinished() )
 	{
 		event.response.setHeader( 'Content-Type', 'text/html' );
 		event.response.statusCode	= 404;
-		event.render( 'not_found', { times: 2, message:'404 NOT FOUND'}, ( err, data )=>{
-			if ( ! err && data && data.length > 0 )
-			{
-				res.end( data );
-			}
+		event.render( 'not_found', { times: 2, message:'404 NOT FOUND'}, ( err )=>{
+			if ( err )
+				event.serverError( err );
 		});
 	}
-})
+});
+
