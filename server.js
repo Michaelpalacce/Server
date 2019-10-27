8b309f08071d7329c1d3827b820e74ed1524caa4
Michaelpalacce <SaveTheFuture123>
2019-10-27 13:20:05 +0200

    [master] Moved the rate limits after the timeout and static resources, after 9.1.1

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ require( './handlers/main/bootstrap_plugins' );
 
 server.apply( 'er_static_resources', { paths : [process.env.STATIC_PATH, 'favicon.ico'] } );
 server.apply( 'er_timeout', { timeout : process.env.REUQEST_TIMEOUT } );
+server.apply( 'er_rate_limits' );
 server.apply( 'er_logger' );
 server.apply( 'er_cache_server' );
 server.apply( 'er_response_cache' );
 server.apply( 'er_file_stream' );
 server.apply( 'er_templating_engine' );
-server.apply( 'er_rate_limits' );
 
 server.apply( 'er_session', { ttl: 60 * 60 } );
 server.apply( 'er_body_parser_multipart' );
