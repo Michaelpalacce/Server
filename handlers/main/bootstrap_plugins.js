f7802674ec006512ee14c49a573ff5d219643a1e
Michaelpalacce <SaveTheFuture123>
2020-02-25 21:14:01 +0200

    [master] Implemented newest version of event_request

--- handlers/main/bootstrap_plugins.js
+++ handlers/main/bootstrap_plugins.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const ejs													= require( 'ejs' );
-const path													= require( 'path' );
-const { Loggur, LOG_LEVELS, Server, Development, Logging }	= require( 'event_request' );
-const { MultipartFormParser }								= Development.BodyParserHandler;
-const PROJECT_ROOT											= path.parse( require.main.filename ).dir;
-const { Console, File }										= Logging;
+const ejs										= require( 'ejs' );
+const path										= require( 'path' );
+const { Loggur, LOG_LEVELS, Server, Logging }	= require( 'event_request' );
+const PROJECT_ROOT								= path.parse( require.main.filename ).dir;
+const { Console, File }							= Logging;
 
 let transports	= [
 	new File({
@@ -39,10 +38,10 @@ let logger	= Loggur.createLogger({
 
 const server					= Server();
 const PluginManager				= server.getPluginManager();
-const templatingEnginePlugin	= PluginManager.getPlugin( 'er_templating_engine' );
-const cacheServerPlugin			= PluginManager.getPlugin( 'er_cache_server' );
-const multipartBodyParserPlugin	= PluginManager.getPlugin( 'er_body_parser_multipart' );
-const loggerPlugin				= PluginManager.getPlugin( 'er_logger' );
+const templatingEnginePlugin	= PluginManager.getPlugin( server.er_templating_engine );
+const cacheServerPlugin			= PluginManager.getPlugin( server.er_cache_server );
+const multipartBodyParserPlugin	= PluginManager.getPlugin( server.er_body_parser_multipart );
+const loggerPlugin				= PluginManager.getPlugin( server.er_logger );
 
 const dataServer				= cacheServerPlugin.getServer();
 process.cachingServer			= dataServer;
@@ -50,9 +49,7 @@ process.cachingServer			= dataServer;
 dataServer.set( process.env.ADMIN_USERNAME, { password : process.env.ADMIN_PASSWORD, route : '\\' }, -1 );
 
 templatingEnginePlugin.setOptions( { templateDir : path.join( PROJECT_ROOT, process.env.TEMPLATING_DIR ), engine : ejs } );
-multipartBodyParserPlugin.setOptions({
-	parsers: [{ instance : MultipartFormParser, options : { tempDir : path.join( PROJECT_ROOT, process.env.UPLOADS_DIR ) } }]
-});
+multipartBodyParserPlugin.setOptions( { tempDir : path.join( PROJECT_ROOT, process.env.UPLOADS_DIR ) } );
 
 loggerPlugin.setOptions({ logger });
 Loggur.addLogger( logger );
--- server.js
+++ server.js
@@ -14,17 +14,20 @@ server.apply( 'er_env' );
 // Configure the plugins
 require( './handlers/main/bootstrap_plugins' );
 
-server.apply( 'er_static_resources', { paths : [process.env.STATIC_PATH, 'favicon.ico'] } );
-server.apply( 'er_timeout', { timeout : process.env.REUQEST_TIMEOUT } );
-server.apply( 'er_rate_limits' );
-server.apply( 'er_logger' );
-server.apply( 'er_cache_server' );
-server.apply( 'er_response_cache' );
-server.apply( 'er_file_stream' );
-server.apply( 'er_templating_engine' );
+server.apply( server.er_static_resources, { paths : [process.env.STATIC_PATH, 'favicon.ico'] } );
+server.apply( server.er_body_parser_form );
+server.apply( server.er_body_parser_json );
+server.apply( server.er_body_parser_multipart );
+server.apply( server.er_timeout, { timeout : process.env.REUQEST_TIMEOUT } );
+server.apply( server.er_rate_limits );
+server.apply( server.er_logger );
+server.apply( server.er_cache_server );
+server.apply( server.er_response_cache );
+server.apply( server.er_file_stream );
+server.apply( server.er_templating_engine );
 
-server.apply( 'er_session' );
-server.apply( 'er_body_parser_multipart' );
+server.apply( server.er_session );
+
 
 // Handlers
 server.add( handlers );
