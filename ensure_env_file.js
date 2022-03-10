b1ebfdb2e818da711d088d9de33709256f238338
Stefan Genov <stefantigro@gmail.com>
2022-03-10 02:43:14 +0200

    [master] Progress

--- ensure_env_file.js
+++ ensure_env_file.js
@@ -1,15 +1,13 @@
 const fs			= require( 'fs' );
 const path			= require( 'path' );
 const configPath	= require( './api/main/utils/config_path' );
-
-const PROJECT_ROOT	= path.parse( require.main.filename ).dir;
 const envFile		= path.join( configPath, 'env.js' );
 
 if ( ! fs.existsSync( configPath ) )
 	fs.mkdirSync( configPath, { recursive: true } );
 
 if ( ! fs.existsSync( path.join( configPath, 'env.js' ) ) )
-	fs.copyFileSync( path.join( PROJECT_ROOT, 'env.js.template' ), envFile );
+	fs.copyFileSync( 'env.js.template', envFile );
 
 module.exports	= envFile;
 
